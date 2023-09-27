78dcda7fa811ca036a609e09cf3e47d49a1d1db1
hummingjay <jays7mill@gmail.com>
2023-09-27 19:05:02 +0300

    documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
+
+
 def pascal_triangle(n):
+    """
+    function that returns a pascal triangle in form of lists
+    """
     triangle = []
     if (n <= 0):
         return []
