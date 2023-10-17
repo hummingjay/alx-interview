3e6ddef409e4954a21e5ab01eb18a039ceb6f880
hummingjay <jays7mill@gmail.com>
2023-10-17 07:08:41 +0300

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,6 +4,7 @@ Script that computes to find least number of operations
 Uses only two operation Copy all and Paste
 """
 
+
 def minOperations(n):
     """
     Method that takes in an integer n
@@ -16,7 +17,7 @@ def minOperations(n):
 
     for num in range(2, n + 1):
         i += 1
-        if n % i == 0: # check if can copy and paste
+        if n % i == 0:  # check if can copy and paste
             while n % i == 0:
                 n /= i
                 ans_list.append(i)
